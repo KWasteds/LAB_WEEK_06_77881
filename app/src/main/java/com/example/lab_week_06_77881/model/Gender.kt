761f0b377cd35419008325c6af41104b1b3f3373
KWasteds <kelvinjo2008@gmail.com>
2025-10-01 15:29:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_06_77881/model/Gender.kt
@@ -0,0 +1,5 @@
+package com.example.lab_week_06_77881.model
+
+enum class Gender {
+    Female, Male, Unknown
+}
---
+++ app/src/main/java/com/example/lab_week_06_77881/model/ImageLoader.kt
@@ -0,0 +1,5 @@
+package com.example.lab_week_06_77881.model
+
+interface ImageLoader {
+    void loadImage(imageUrl: String, imageView: ImageView)
+}
